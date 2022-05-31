e08e12216b6181f6a5b5c615b842c2e52ba2c857
AntRanYRGO <antran0410@skola.goteborg.se>
2022-05-31 13:20:20 +0200

    Adding readme.md and fixing check_g script

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+## ReadMe
+
+#### Description
+This is a school assignment modelling an application to manage substitute teachers for schools.
+
+### Build
+Unpack zip file, go to the directory where it was unpacked
+
+#### **Run the compile_servlet_and_start_winstone.sh:** <br>
+**Command for WSL:** <br> ./compile_servlet_and_start_winstone.sh
+
+Servlet should now be up and running on localhost:8080
+
+#### **Check_G**
+To check if the assignment passes the specifications for G, run the check_g.sh script <br>
+**Command for WSL**: <br> 
+./check_g.sh
--- check_g.sh
+++ check_g.sh
@@ -78,34 +78,3 @@ echo Pass!
 
 echo Grade is at least G
 
-echo "If all test below pass, the grade is VG, otherwise see the documentation for how the API handles bad input."
-
-echo
-echo "The requirements for VG is to:"
-echo '* handle incorrect input like missing parameters in a documented way'
-echo '** Example: for a date or substitute_id which cannot be found in the database, the web API could respond with "404 Not Found"'
-echo '* responds with the correct content-type header (e.g. application/json)'
-echo
-
-# Content-Type checks:
-echo "Checking content-type for json..."
-$HEAD "${END_POINT}?format=json" | grep -i content-type | grep 'application/json' || fail 'Content-Type: application/json not found'
-echo Pass!
-
-echo "Checking content-type for xml..."
-$HEAD "${END_POINT}?format=xml" | grep -i content-type | grep 'application/xml' || fail 'Content-Type: application/xml not found'
-echo Pass!
-
-# 404 Not found checks:
-echo "Checking not found (json)..."
-$HEAD "${END_POINT}?format=json&day=apa" | grep '^404' &> /dev/null || fail '404 not sent'
-echo Pass!
-
-echo "Checking Not Found (xml)..."
-$HEAD "${END_POINT}?format=xml&day=apa" | grep '^404' &> /dev/null || fail '404 not sent'
-echo Pass!
-
-# Bad format - Bad Request checks
-echo "Checking Bad Request"
-$HEAD "${END_POINT}?format=apa" | grep '^400' &> /dev/null || fail '400 not sent for bad format'
-echo Pass!
